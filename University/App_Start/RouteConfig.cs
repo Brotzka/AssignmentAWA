0c2948fa613f32426498ff3dbc46c4b95fc2ea76
Brotzka <fabian@faaren.de>
2018-06-08 13:22:14 +0200

    Index-Seite

--- University/App_Start/RouteConfig.cs
+++ University/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace University
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Applicant", action = "AddApplicant", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "index", id = UrlParameter.Optional }
             );
         }
     }
